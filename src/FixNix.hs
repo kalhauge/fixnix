f5cb95c97048e38e864f9bd6f459b2bd1ded3439
Christian Gram Kalhauge <kalhauge@cs.ucla.edu>
2020-05-09 20:57:08 +0200

    Refactor locations into dynamic structures

--- src/FixNix.hs
+++ src/FixNix.hs
@@ -1,20 +1,32 @@
 {-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ViewPatterns #-}
 module FixNix where
 
 -- base 
 import Data.Bifunctor
 import Data.Void
-import Data.Functor
 import Control.Monad
+import System.Exit
+
+-- containers
+import qualified Data.Set as S
 
 -- text
 import qualified Data.Text as Text
+import qualified Data.Text.Lazy as LazyText
+import qualified Data.Text.Lazy.Encoding as LazyText
+--import qualified Data.Text.IO as Text
 
 -- megaparsec
-import Text.Megaparsec
-import Text.Megaparsec.Char
+import qualified Text.Megaparsec      as P
+import qualified Text.Megaparsec.Char as P
+
+-- neat-interpolation
+import NeatInterpolation (text)
 
 -- optparse-applicative
 import Options.Applicative
@@ -22,69 +34,112 @@ import Options.Applicative
 -- path
 import Path
 
--- | The location of the tar-file.
-data Location 
-  = GitHub GitHubConfig GitCommit
-  deriving (Show)
+-- typed-process
+import System.Process.Typed
+
+type P = P.Parsec Void Text.Text
+
+data LocationExample = LocationExample
+  { exampleText :: Text.Text
+  , exampleHelp :: Text.Text
+  }
+
+example :: Text.Text -> Text.Text -> LocationExample
+example = LocationExample
+
+-- | A location type
+data LocationType = LocationType
+  { locationName     :: Text.Text
+  , locationPrefix   :: S.Set Text.Text
+  , locationExamples :: [ LocationExample ]
+  , locationParser   :: P (IO (Text.Text, Maybe Text.Text))
+  }
+
+githubLocation = LocationType
+  { locationName = "GitHub"
+  , locationPrefix = S.fromList [ "gh", "github" ]
+  , locationExamples = 
+    [ example "github:nixos/nixpkgs/tags/20.03" 
+        "accesss the tag of a github page"
+    , example "github:nixos/nixpkgs/heads/nixpkgs-20.03" 
+        "accesss the branch of a github page"
+    , example "gh:nixos/nixpkgs/rev/1234abcd" 
+        "accesss the revision of a github page"
+    ]
+  , locationParser = do
+      gitHubOwner <- P.takeWhile1P Nothing ('/' /=)
+
+      P.char '/'
+
+      gitHubRepo <- P.takeWhile1P Nothing ('/' /=)
+
+      P.char '/'
+
+      commit <- gitCommitP
+
+      return $ do 
+        case commit of
+          GitRevision rev -> do
+            return 
+              ( [text|https://github.com/$gitHubOwner/$gitHubRepo/archive/$rev.tar.gz|]
+              , Just [text|${gitHubRepo}_$txt|]
+              )
+          GitTag tag -> do
+            return 
+              ( [text|https://github.com/$gitHubOwner/$gitHubRepo/archive/$tag.tar.gz|]
+              , Just [text|${gitHubRepo}_$txt|]
+              )
+          -- GitBranch bar -> do
+  }
+
+gitCommitP :: P GitCommit
+gitCommitP = msum 
+  [ GitTag      . Text.pack <$> do
+    P.string' "tags/"  P.<?> "\"tags/\" for tags"
+    P.someTill P.printChar P.eof P.<?> "a tag name"
+
+  , GitBranch   . Text.pack <$> do
+    P.string' "heads/" P.<?> "\"heads/\" for branches"
+    P.someTill P.printChar P.eof P.<?> "a branch name"
+
+  , GitRevision . Text.toLower . Text.pack <$> do
+    P.string' "rev/" P.<?> "\"rev/\" for revisions"
+    P.someTill P.hexDigitChar P.eof P.<?> "a revision"
+  ]
 
 data GitCommit
   = GitBranch    !Text.Text
   | GitTag       !Text.Text
   | GitRevision  !Text.Text
-  deriving (Show)
-
-data GitHubConfig = GitHubConfig
-  { gitHubOwner :: !Text.Text
-  , gitHubRepo  :: !Text.Text
-  }
+--  | GitWildCard  !Text.Text
   deriving (Show)
 
 data AddConfig = AddConfig
   { cfgFixFile  :: !(Path Rel File)
-  , cfgLocation :: !(Location)
+  , cfgLocation :: !(LocationType, Text.Text, Maybe Text.Text)
   , cfgName     :: !(Maybe Text.Text)
   }
-  deriving (Show)
+--   deriving (Show)
 
-readLocation :: ReadM Location
-readLocation = eitherReader 
-  $ first errorBundlePretty . parse locationP "LOCATION"
+readLocation :: [LocationType] -> ReadM (LocationType, Text.Text, Maybe Text.Text)
+readLocation ltps = eitherReader 
+  $ first P.errorBundlePretty . P.parse (locationP ltps) "LOCATION" . Text.pack
 
-locationP :: Parsec Void String Location
-locationP = do 
-    locationParser <- label "location type" $ msum 
-      [ string' "nixos" $> do 
-        GitHub 
-          (GitHubConfig { gitHubOwner = "NixOS", gitHubRepo = "nixpkgs" })
-          <$> gitCommitP 
-      , string' "github" $> do
-        GitHub <$> (gitHubConfigP <* char '/') <*> gitCommitP
-      ]
-    char ':'
-    l <- locationParser
-    eof
-    return l
- where
-  gitCommitP = msum 
-    [ GitTag      . Text.pack <$> (string' "tags/" *> takeWhileP Nothing (':' /=))
-    , GitBranch   . Text.pack <$> (string' "heads/" *> takeWhileP Nothing (':' /=))
-    , GitRevision . Text.toLower . Text.pack <$> Text.Megaparsec.many hexDigitChar
+locationP :: [LocationType] -> P (LocationType, Text.Text, Maybe Text.Text)
+locationP ltps = msum 
+    [ do
+      P.label "location type" $ 
+        msum [ P.string' prfx | prfx <- S.toList locationPrefix ]
+      P.char ':'
+      (url, name) <- locationParser
+      return (lt, url, name)
+    | lt@(LocationType {..}) <- ltps
     ]
+ where
 
-  gitHubConfigP = do
-    gitHubOwner <- Text.pack <$>
-      takeWhileP Nothing ('/' /=)
-
-    char '/'
-
-    gitHubRepo <- Text.pack <$>
-      takeWhileP Nothing ('/' /=)
-
-    pure $ GitHubConfig { gitHubOwner, gitHubRepo }
-
-parseAddConfig :: Parser AddConfig
-parseAddConfig = do
-  cfgLocation <- argument readLocation $ 
+parseAddConfig :: [LocationType] -> Parser AddConfig
+parseAddConfig ltps = do
+  cfgLocation <- argument (readLocation ltps) $ 
     metavar "LOCATION"
     <> help "the location to download"
 
@@ -103,19 +158,65 @@ parseAddConfig = do
     , cfgName
     }
 
-readConfig :: IO AddConfig 
-readConfig = execParser $ 
-  info (parseAddConfig <**> helper)
+readConfig :: [LocationType] -> IO AddConfig 
+readConfig ltps = execParser $ 
+  info (parseAddConfig ltps <**> helper)
     ( fullDesc
     <> header "fixnix - a nix version fixer"
     )
 
+newtype Sha256 = Sha256 
+  { sha256AsText :: Text.Text }
+
+zeroSha256 :: Sha256
+zeroSha256 = 
+  Sha256 "0000000000000000000000000000000000000000000000000000"
+
+data FetchUrl = FetchUrl
+  { fetchUrlUrl       :: Text.Text
+  , fetchUrlName      :: Maybe Text.Text
+  , fetchUrlUnpack    :: Bool
+  } 
+  deriving (Show)
+
+renderFetchUrl :: FetchUrl -> Sha256 -> Text.Text
+renderFetchUrl FetchUrl {..} (sha256AsText -> sha256) =
+  case fetchUrlUnpack of 
+    True -> [text|builtins.fetchurl {$name
+              url    = "$fetchUrlUrl";
+              sha256 = "$sha256";
+            }|]
+    False -> ""
+ where
+  name = case fetchUrlName of 
+    Just n -> "\n  " <> [text|name   = "$n";|] 
+    Nothing -> ""
+
+prefetchArgs :: FetchUrl -> [ String ]
+prefetchArgs FetchUrl {..} = Text.unpack <$> concat 
+  [ [ "--unpack" | fetchUrlUnpack ]
+  , maybe [] (\name -> [ "--name", name ]) fetchUrlName
+  , [ fetchUrlUrl ]
+  ]
+
+prefetchIO :: FetchUrl -> IO (Maybe Sha256)
+prefetchIO furl = do
+  (ec, out) <- readProcessStdout . proc "nix-prefetch-url" $
+    prefetchArgs furl
+  return $ case ec of 
+    ExitSuccess   -> Just (Sha256 . LazyText.toStrict $ LazyText.decodeUtf8 out)
+    ExitFailure _ -> Nothing
+
+locations = [ githubLocation ]
+
 run :: IO ()
 run = do
-  cfg <- readConfig
-
-  path
-
-
-  print cfg
+  cfg <- readConfig locations
+  return ()
+  -- case cfgLocation cfg of 
+  --   GitHub gf gc  -> FetchUrl 
+  --     { fetchUrlUrl :: [text|http://github.com/$|]
+  --     , fetchUrlName = 
+  --     }
+  -- prefetchIO
 
--- test/FixNixSpec.hs
+++ test/FixNixSpec.hs
@@ -1,35 +1,80 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE QuasiQuotes #-}
 module FixNixSpec where
 
 import Test.Hspec
 import Test.Hspec.Megaparsec
 
+import NeatInterpolation (text)
+
 import Text.Megaparsec
+
+import Control.Monad
+import qualified Data.Text as Text
 
 import FixNix
 
 spec :: Spec 
 spec = do
   describe "location parser" do
-    let 
-      itShouldSucceedOn s = 
-        it ("should succeed on " ++ show s) do
-          parse locationP "" `shouldSucceedOn` s
+    forM_ locations \LocationType {..} -> do
+      describe (Text.unpack locationName) do
+        forM_ locationExamples \LocationExample {..} ->  
+          it ("should succeed on " ++ show exampleText) do
+            parse locationParser "" `shouldSucceedOn` exampleText
+      -- let 
+      --   itShouldSucceedOn s = 
+      --     it ("should succeed on " ++ show s) do
+      --       parse locationParser "" `shouldSucceedOn` s
 
-      itShouldFailOn s = 
-        it ("should fail on " ++ show s) do
-          parse locationP "" `shouldFailOn` s
+      --   itShouldFailOn s = 
+      --     it ("should fail on " ++ show s) do
+      --       parse locationParser "" `shouldFailOn` s
 
-    describe "nixos" do
-      itShouldSucceedOn "nixos:tags/20.03" 
-      itShouldSucceedOn "nixos:heads/nixpkgs-20.03" 
-      itShouldSucceedOn "nixos:AB023ad"
-      itShouldSucceedOn "nixos:20.03"
 
-      itShouldFailOn "nixos:a0232s"
-      itShouldFailOn "nixos:eads/nixpkgs-20.03" 
+      -- describe "nixos" do
+      --   itShouldSucceedOn "nixos:tags/20.03" 
+      --   itShouldSucceedOn "nixos:heads/nixpkgs-20.03" 
+      --   itShouldSucceedOn "nix:heads/nixpkgs-20.03" 
 
-    describe "github" do
-      itShouldSucceedOn "github:nixos/nixpkgs/tags/20.03" 
-      itShouldSucceedOn "github:nixos/nixpkgs/20.03" 
+      --   -- itShouldSucceedOn "nixos:AB023ad"
+      --   -- itShouldSucceedOn "nixos:20.03"
+
+      --   itShouldFailOn "nixos:tag/20"
+      --   itShouldFailOn "nixos:eads/nixpkgs-20.03" 
+      --   itShouldFailOn "nixos:tags/"
+
+      -- describe "github" do
+      --   itShouldSucceedOn "github:nixos/nixpkgs/tags/20.03" 
+      --   itShouldSucceedOn "github:nixos/nixpkgs/heads/nixpkgs-20.03" 
+      --   itShouldSucceedOn "gh:kalhauge/fixnix/rev/abcdef" 
+
+  describe "renderFetchUrl" do
+    it "should print something" do
+      let 
+        input = FetchUrl 
+          { fetchUrlUrl = "hello"
+          , fetchUrlName = Just "Here"
+          , fetchUrlUnpack = True
+          }
+        output = [text|builtins.fetchurl {
+          name   = "Here";
+          url    = "hello";
+          sha256 = "0000000000000000000000000000000000000000000000000000";
+        }|]
+      renderFetchUrl input zeroSha256 `shouldBe` output
+    it "should be able to not print name" do
+      let 
+        input = FetchUrl 
+          { fetchUrlUrl = "hello"
+          , fetchUrlName = Nothing
+          , fetchUrlUnpack = True
+          }
+        output = [text|builtins.fetchurl {
+          url    = "hello";
+          sha256 = "0000000000000000000000000000000000000000000000000000";
+        }|]
+      renderFetchUrl input zeroSha256 `shouldBe` output
 
